3a05aafc2f53fd5bf39d2c16173b21cc0dcc4995
thedarik <thedarik2021@gmail.com>
2025-03-18 13:17:59 +0500

    Initial commit

---
+++ bin/dart_backent.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
+Future<Response> getUsers(Request request) async {
+  final file = File('bin/users.json');
+
+  if (await file.exists()) {
+    String content = await file.readAsString();
+    return Response.ok(content, headers: {'Content-Type': 'application/json'});
+  } else {
+    return Response.notFound(jsonEncode({'error': 'Fayl topilmadi'}), headers: {'Content-Type': 'application/json'});
+  }
+}
+
+void main() async {
+  final handler = Pipeline().addHandler((Request request) {
+    if (request.url.path == 'bin/users.json') {
+      return getUsers(request);
+    }
+    return Response.notFound('Sahifa topilmadi');
+  });
+
+  final server = await io.serve(handler, InternetAddress.anyIPv4, 8080);
+  print('Server ishga tushdi: http://${server.address.host}:${server.port}');
+}
